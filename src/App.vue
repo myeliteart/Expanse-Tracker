b94d284b8969f7f01a01ae5f5988dbc743f041f5
myeliteart <eliteny0@gmail.com>
2025-10-06 01:28:10 -0400

    update app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="relative sm:min-h-screen flex sm:flex-row flex-col justify-between w-full" :class="store.isDarkMode == false ? 'bg-white text-gray-800' : 'bg-gray-800 text-white'">
+  <div class="relative min-h-screen flex sm:flex-row flex-col justify-between w-full" :class="store.isDarkMode == false ? 'bg-white text-gray-800' : 'bg-gray-800 text-white'">
     <the-nav></the-nav>
     <div class="relative text-end mt-4 mr-4 sm:absolute sm:top-5 sm:right-8 cursor-pointer" @click="store.toggleDark">
       <font-awesome-icon class="" :icon="['fas', 'circle-half-stroke']"></font-awesome-icon> 
