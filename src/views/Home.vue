dfb94538dd5a9ff57cbb4378067f4a25b14da71c
myeliteart <eliteny0@gmail.com>
2025-10-09 12:18:33 -0400

    update home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
 <div class="justify-between gap-14 lg:gap-24 flex flex-col lg:flex-row items-top mx-auto">
     <div class="w-full lg:w-[65%]">
         <div class="flex items-center justify-center">
-             <font-awesome-icon :icon="['fas', 'calculator']" :class="store.isDarkMode == false ? 'text-4xl text-gray-500' : 'text-white text-3xl'"></font-awesome-icon> 
+             <font-awesome-icon :icon="['fas', 'calculator']" :class="store.isDarkMode == false ? 'text-4xl text-gray-500' : 'text-white text-4xl'"></font-awesome-icon> 
              <h2 class="text-2xl font-medium ml-6">Your Balance: <span class="font-bold text-3xl">${{ Number(store.balance || 0).toFixed(2) }}</span></h2>
         </div>
         <hr class="mt-2 border-gray-400"></hr>
